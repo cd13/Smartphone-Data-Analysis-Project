f8e2e9b8635c6c67ac660cc33ccc109f017001d5
cd13 <carodemers@hotmail.com>
2014-06-19 10:25:32 +0200

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,60 @@
-Smartphone-Data-Analysis-Project
-================================
+# Getting and Cleaning Data Course Project
+===================
 
-Samsung smartphone UCI HAR dataset used for Getting and Cleaning Data project course
+## 1. Introduction
+
+This repository contains the Coursera `Getting and Cleaning Data` course project, which is using the UCI Human Activity Recognition Using Smartphones Dataset.
+
+The purpose of this project is to demonstrate the collection, work with, and cleaning of this dataset. The end product is a tidy data file uploaded onto the Coursera site that can be used for later analysis and will be peer assessed.
+
+This `README.md` file presents details of the files in this repository.
+
+---
+## 2. Original Data Source
+
+All information concerning the source of the data and the experiments performed to obtain the data can be found here:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+---
+## 3. Files in Repository
+
+This repo includes the following files
+- `README.md`
+- `run_analysis.R`
+- `CodeBook.md`
+
+---
+### 3.1. File `run_analysis.R`
+
+This file contains the R code used to obtain the tidy data file called `smartphonetidydataset.txt`.
+
+The script was generated in RStudio Version 0.98.501.
+
+To run the script, it must be downloaded and then sourced (`source(run_analysis.R)`) from your working directory in R. It is also necessary to install the `plyr` package to run this script.
+
+Briefly the script:
+
+- Downloads the dataset from the URL mentioned in section 2 and unzips it to create an UCI HAR Dataset folder in your working directory.
+- Reads in the files containing the test and train data, creates a data frame for each, and merges both data frames into one.
+- Extracts a subset of data from the data frame where the processed data variable names contain the string `mean` or `std`. The `meanFreq` and angle variables are included, resulting in a total of `88` variables, including the factor variables `subject` and `activity`.
+- Appropriately labels the data frame with descriptive activity names in place of activity ids.
+- Updates the processed data variable names in the subsetted data frame to improve readibility.
+- Creates a new tidy data frame containing the average of processed data variable for each subject and each activity. This tidy data frame has `180 observations` and `88 columns`. 
+- Writes the data in a table in the above format under the file `smartphonetidydataset.txt`. To read the file, the separator `,` is required.
+In R, the following code can be used, if the file is in the R working directory: 
+
+```
+read.table("smartphonetidydataset.txt",sep=",",header=TRUE,stringsAsFactors=TRUE)
+```
+
+This data file meets the principles of a tidy dataset since each variable forms a column, each observation forms a row and it contains information on the mean of the processed data variables.
+
+---
+### 3.2. File `CodeBook.md`
+The codebook file describes the variables, the data, and any transformations and work performed to clean up the data.
+
+---
+### 4. Acknowledgements
+
+Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto. Smartlab - Non Linear Complex Systems Laboratory DITEN - Università degli Studi di Genova. Via Opera Pia 11A, I-16145, Genoa, Italy. activityrecognition@smartlab.ws www.smartlab.ws
---
+++ run_analysis.R
@@ -0,0 +1,83 @@
+
+#### STEP 1 ####
+
+library(plyr)
+
+# get the file url and download it into the current working directory,
+today = as.Date(Sys.time())
+fileurl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+# create a temporary directory
+td = tempdir()
+# create the placeholder file
+tf = tempfile(tmpdir=td, fileext=".zip")
+# download into the placeholder file
+download.file(fileurl, tf)
+
+# List all files names inside of the zipped folder.
+filelist <- as.character(unzip(tf, list = TRUE)$Name)
+
+# First place the files names subject_train.txt, x_train.txt and y_train.txt from the zipped folder into a dataset.
+# # Files are placed in this order. Column order will be changed later to move the activity column (y_train.txt) to the 2nd column.
+# Then read the data from the above mentionned files and merge them column by column.
+trainfilelist <- grep(pattern = "^(.*)(train/)[sXy](.*)(_train)", filelist, value=TRUE)
+traindataset <- do.call("cbind",lapply(trainfilelist, FUN=function(files){read.table(files, header=FALSE, stringsAsFactors=FALSE)}))
+
+# First place the files names subject_test.txt, x_test.txt and y_test.txt from the zipped folder into a dataset.
+# Files are placed in this order. Column order will be changed later to move the activity column (y_test.txt) to the 2nd column.
+# Then read the data from the above mentionned files and merge them column by column.
+testfilelist <- grep(pattern = "^(.*)(test/)[sXy](.*)(_test)", filelist, value=TRUE)
+testdataset <- do.call("cbind",lapply(testfilelist, FUN=function(files){read.table(files, header=FALSE, stringsAsFactors=FALSE)}))
+
+# Read in the feature.txt file to use as column names for the test and training datasets, as well as the subject
+# (subject_train/text.txt file read first in dataframe) and activity (y_train/test.txt file read last in dataframe).
+columnnames <- read.table(filelist[2], colClasses=c("NULL","character"), header=FALSE)
+firstcolumn <- c("subject")
+lastcolumn <- c("activity")
+allcolumnnames = t(rbind(firstcolumn, columnnames, lastcolumn))
+
+# Assigning column names to both the train and test datasets.
+names(traindataset) <- allcolumnnames
+names(testdataset) <- allcolumnnames
+
+# Merging row-wise the train and test datasets into one dataset.
+fulldataset <- rbind(traindataset,testdataset)
+
+#### STEP 2 ####
+
+# Selecting the columns of data with word "mean" (including "meanFreq" and "gravityMean"), "std" as well as the
+# "activity" and "subject" columns.
+subdataset <- fulldataset[grepl('mean|std|activity|subject', names(fulldataset), ignore.case=TRUE)]
+
+# Moving "activity" column from last column to second column position.
+col_idx <- grep("activity", names(subdataset))
+subdataset <- subdataset[, c(col_idx, (1:ncol(subdataset))[-col_idx])]
+
+#### STEP 3 ####
+
+# Making both the subject and activity variables as factors.
+# Substituting  the factors 1 to 6 of the "activity" column for descriptive strings from "activity_labels.txt" file.
+subdataset$activity <- as.factor(subdataset$activity)
+subdataset$subject <- as.factor(subdataset$subject)
+subdataset <- arrange(subdataset,subdataset$subject,subdataset$activity)
+levels(subdataset$activity) <- c("Walking", "WalkingUpstairs","WalkingDownstairs","Sitting","Standing","Laying")
+
+#### STEP 4 ####
+
+# Changing the column names to remove metacharacters.
+names(subdataset) <- gsub(pattern="-",replacement="", x=names(subdataset))
+names(subdataset) <- gsub(pattern="std",replacement="Std", x=names(subdataset))
+names(subdataset) <- gsub(pattern="mean",replacement="Mean", x=names(subdataset))
+names(subdataset) <- gsub(pattern="\\()",replacement="", x=names(subdataset))
+names(subdataset) <- sub(pattern="\\(",replacement="OF", x=names(subdataset))
+names(subdataset) <- sub(pattern="\\,",replacement="AND", x=names(subdataset))
+names(subdataset) <- gsub(pattern="\\)",replacement="", x=names(subdataset))
+names(subdataset) <- sub(pattern="BodyBody",replacement="Body", x=names(subdataset))
+
+#### STEP 5 ####
+
+# Creating a second dataset "meandataset" to calculate the average (mean) of the measurements by subject and activity.
+meandataset <- setNames(aggregate(. ~ subject+activity, subdataset, function(x) mean = mean(x)), c(sapply("Avg", paste0, names(subdataset))))
+names(meandataset)[1:2] <- c("subject", "activity")
+
+# Writing results of average (mean) calculations in a table.
+write.table(meandataset, file = "smartphonetidydataset.txt", sep = ",", quote = FALSE, qmethod = "double")
